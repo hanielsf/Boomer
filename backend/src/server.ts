a4f8f606e49101f4d2814589459b24094421fc74
Haniel S.F <haniel.cesa@gmail.com>
2024-09-03 01:24:42 -0300

    v3.0.2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,7 @@
 import __init from "./app";
 import { logger } from "./utils/logger";
-import { CronJob } from 'cron';
-import CheckDelayedTickets from "./services/TicketServices/CheckDelayedTickets";
 
 __init().then((app: any) => {
   app.start();
   logger.info("Started system!!");
 });
-
-const job = new CronJob("*/5 * * * *", CheckDelayedTickets);
-job.start()
